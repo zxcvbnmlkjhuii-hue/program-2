6a8124a1bebd7bf4bbcea8f00967f8353b397028
zxcvbnmlkjhuii-hue <zxcvbnmlkjhuii@gmail.com>
2026-01-20 21:43:42 +0900

    (2025.01.20) 열거형

--- program/source.c
+++ program/source.c
@@ -2,12 +2,73 @@
 #include <stdlib.h>
 #include <windows.h>
 
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
 
+
+void position(int x, int y)
+{
+	COORD position = { x, y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),position );
+
+}
 
 int main()
 {
+	
+	
+	char key = 0;
+	int x = 5, y = 5;
+	while (1)
+	{
+		system("cls");
+		position(x, y);
+		printf("★");
 
-	printf("system ");
+		int key = _getch();
+		if (key == 224)
+		{
+
+			key = _getch();
+			switch (key)
+			{
+			case UP:
+				
+				y--;
+				break;
+			case LEFT:
+				
+				x--;
+				break;
+			case RIGHT:
+			
+				x++;
+				break;
+			case DOWN:
+			
+				y++;
+				break;
+			default:
+				
+				
+				break;
+			}
+		}
+		else
+		{
+			
+			
+		}
+
+		
+		
+	}
+
+	
+		// 커서의 위치를 이동시키는 함수
 
 	return 0;
 }

d54c5082da105f460454951db033fa6881bd90a5
zxcvbnmlkjhuii-hue <zxcvbnmlkjhuii@gmail.com>
2026-01-21 21:44:29 +0900

    (2026.01.21) 게임 설계론

--- program/source.c
+++ program/source.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
 #include <windows.h>
 
 #define UP 72
@@ -7,69 +7,127 @@
 #define RIGHT 77
 #define DOWN 80
 
+int screenIndex;
+HANDLE screen[2];
 
-void position(int x, int y)
+void Initialize()
 {
+	CONSOLE_CURSOR_INFO cursor;
+
+	// 화면 버퍼를 2개 생성합니다.
+	screen[0] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	screen[1] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	cursor.bVisible = FALSE;
+
+	SetConsoleCursorInfo(screen[0], &cursor);
+	SetConsoleCursorInfo(screen[1], &cursor);
+}
+
+void Flip()
+{
+	SetConsoleActiveScreenBuffer(screen[screenIndex]);
+
+	screenIndex = !screenIndex;
+}
+
+void Clear()
+{
+	COORD position = { 0, 0 };
+
+	DWORD dword;
+
+	CONSOLE_SCREEN_BUFFER_INFO buffer;
+
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	GetConsoleScreenBufferInfo(console, &buffer);
+
+	int width = buffer.srWindow.Right - buffer.srWindow.Left + 1;
+	int height = buffer.srWindow.Bottom - buffer.srWindow.Top + 1;
+
+	FillConsoleOutputCharacter
+	(
+		screen[screenIndex], ' ', width * height, position, &dword
+	);
+}
+
+void Release()
+{
+	CloseHandle(screen[0]);
+	CloseHandle(screen[1]);
+}
+
+void Render(int x, int y, const char* character)
+{
+	DWORD dword;
 	COORD position = { x, y };
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),position );
-
+	SetConsoleCursorPosition(screen[screenIndex], position);
+	WriteFile(screen[screenIndex], character, strlen(character), &dword, NULL);
 }
+
 
 int main()
 {
-	
-	
 	char key = 0;
-	int x = 5, y = 5;
+	
+	CONSOLE_SCREEN_BUFFER_INFO console;
+	HANDLE heoght = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(heoght, &console);
+
+	int width = console.srWindow.Right - console.srWindow.Left-2;
+	int height = console.srWindow.Bottom - console.srWindow.Top ;
+
+	int x = 0;
+	int y = 0;
+
+	Initialize();
+
+	Render(x, y, "☆");
+
 	while (1)
 	{
-		system("cls");
-		position(x, y);
-		printf("★");
+		Flip();
 
-		int key = _getch();
-		if (key == 224)
+		Clear();
+
+		key = _getch();
+
+		if (key == -32 || key == 0)
 		{
-
 			key = _getch();
+		}
+	
 			switch (key)
 			{
-			case UP:
-				
-				y--;
+			case UP:if (y>0)y--;
 				break;
-			case LEFT:
-				
-				x--;
+
+			case LEFT: if (x > 0)x -= 2;
 				break;
-			case RIGHT:
-			
-				x++;
+
+			case RIGHT: if (width > 0)x += 2;
 				break;
-			case DOWN:
-			
-				y++;
+
+			case DOWN: if (width > 0)y++;
 				break;
-			default:
-				
-				
+
+			default: printf("exception\n");
 				break;
 			}
-		}
-		else
-		{
-			
-			
-		}
-
-		
-		
-	}
-
 	
-		// 커서의 위치를 이동시키는 함수
+				Render(x, y, "☆");
+	}
 
 	return 0;
 }
-

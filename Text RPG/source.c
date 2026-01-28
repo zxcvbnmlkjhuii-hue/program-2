260b4a20fa57703c6802a7d545200d2d1bfcbdb1
zxcvbnmlkjhuii-hue <zxcvbnmlkjhuii@gmail.com>
2026-01-28 21:45:48 +0900

    (2026.01.28) 텍스트RPG

--- Text RPG/source.c
+++ Text RPG/source.c
@@ -9,6 +9,11 @@ enum State
 	Action,
 	Battle
 };
+enum Turn
+{
+	PLAYER_TURN,
+	MONSTER_TURN
+};
 struct Player
 {
 	char name[20];
@@ -49,24 +54,46 @@ void ShowPlayerState(struct Player player)
 {
 	printf("---------------------------------\n");
 	printf("이름:%s\n", player.name);
-	printf("체력:%d  ", player.hp);
-	printf("공격력:%d  ", player.attack);
-	printf("방어력:%d  \n", player.defense);
 	printf("레벨:%d  ", player.level);
 	printf("경험치:%d  ", player.exp);
 	printf("필요 경험치:%d\n", player.nextExp);
+	printf("체력:%d  ", player.hp);
+	printf("공격력:%d  ", player.attack);
+	printf("방어력:%d  \n", player.defense);
 	printf("골드:%d\n", player.gold);
 	printf("---------------------------------\n");
 }
 
+void ShowMonsterState(struct Monster monster)
+{
+	printf("---------------------------------\n");
+	printf("이름:%s\n", monster.name);
+	printf("레벨:%d  \n", monster.level);
+	printf("체력:%d  ", monster.hp);
+	printf("공격력:%d  ", monster.attack);
+	printf("방어력:%d  \n", monster.defense);
+	printf("---------------------------------\n");
+}
+
+void ShowBattle()
+{
+	printf("1.공격한다\n");
+	printf("2.방어한다\n");
+	printf("3.도망친다\n");
+}
 
 void main()
 {
 	enum State state = Start;
 	struct Player player = { "기사",100,10,3,1,0,100,50 };
 	struct Monster monster;
-
+	enum Turn turn = PLAYER_TURN;
 	srand(time(NULL));
+	int PlainmonsterType = rand() % 2;
+	if (PlainmonsterType == 1)
+		monster = (struct Monster){ "슬라임",30,5,1,1,20,10 };
+	else if (PlainmonsterType == 0)
+		monster = (struct Monster){ "고블린",40,3,1,1,20,10 };
 	int random = rand();
 	while (1)
 	{
@@ -114,15 +141,45 @@ void main()
 			state = Start;
 			break;
 		case Action:
-			monster = (struct Monster){ "슬라임",30,5,1,1,20,10 };
+
 			system("cls");
 			ShowPlayerState(player);
-			printf("%s과 마주쳤습니다!\n",monster.name);
+			printf("%s과 마주쳤습니다!\n", monster.name);
 			state = Battle;
 			break;
 		case Battle:
 			system("cls");
 			ShowPlayerState(player);
+			ShowMonsterState(monster);
+			ShowBattle();
+
+			if (turn == PLAYER_TURN)
+			{
+				input = 0;
+				scanf_s("%d", &input);
+				if (input == 1)
+				{
+					monster.hp -= player.attack;
+					break;
+				}
+				else if (input == 2)
+				{
+
+				}
+				else if (input == 3)
+				{
+					state = Start;
+					printf("마을로 이동 합니다.\n");
+					break;
+				}
+				turn = MONSTER_TURN;
+			}
+			else if (turn == MONSTER_TURN)
+			{
+
+			}
+
+		
 			printf("전투를 시작합니다!\n");
 		
 			break;
